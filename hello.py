3c30b0a05551d54f790606c4ef944a1dd45ea1fd
micodepp <792293896@qq.com>
2017-08-14 11:29:58 +0800

    qq

---
+++ hello.py
@@ -0,0 +1,13 @@
+#coding:utf-8
+import cgi
+print('Content-Type=text:text/html')
+print()
+print('<html>')
+print('<head>')
+print('<meta charset="utf-8">')
+print('<title>CGI</title>')
+print('</head>')
+print('<body>')
+print('<h2>hello world</h2>')
+print('</body>')
+print('</html>')
